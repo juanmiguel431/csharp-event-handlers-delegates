9d7897f120a8290c19a7256e6071f06542fcd1f1
juanmiguel431 <juanmiguel431@gmail.com>
2022-09-05 19:06:59 -0400

    Async operations in Wpf App

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿namespace WpfApplication1
+﻿using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApplication1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +14,39 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var task = DownloadHtmlAsync("https://en.wikipedia.org/wiki/Car");
+            task.Wait();
+            MessageBox.Show("Completed");
+        }
+
+        private static void DownloadHtml(string url)
+        {
+            var client = new WebClient();
+            var html = client.DownloadString(url);
+
+            Thread.Sleep(5000);
+
+            using (var streamWriter = new StreamWriter(@"C:\work\udemy\C# Advance\result.html"))
+            {
+                streamWriter.Write(html);
+            }
+        }
+        
+        private static async Task DownloadHtmlAsync(string url)
+        {
+            var client = new WebClient();
+            var html = await client.DownloadStringTaskAsync(url);
+            
+            await Task.Delay(5000);
+            
+            using (var streamWriter = new StreamWriter(@"C:\work\udemy\C# Advance\result.html"))
+            {
+                await streamWriter.WriteAsync(html);
+            }
         }
     }
 }
